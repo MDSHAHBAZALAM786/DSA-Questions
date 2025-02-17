bbe375e2ecb88ce87623373b31c8250be0dc5885
Md shahbaz alam <112703427+MDSHAHBAZALAM786@users.noreply.github.com>
2025-02-17 18:59:30 +0530

    Time: 0 ms (100%), Space: 8.3 MB (99.98%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0 || x == 1) return x;  // Base cases
+        
+        int left = 1, right = x / 2, ans = 0;
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            long long square = mid * mid;
+            
+            if (square == x) 
+                return mid;
+            else if (square < x) {
+                ans = mid;  // Store the potential answer
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
