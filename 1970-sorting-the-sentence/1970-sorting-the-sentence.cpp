9a9167aacc979a9b42b3582cb47cce3c68828783
Md shahbaz alam <112703427+MDSHAHBAZALAM786@users.noreply.github.com>
2025-02-28 17:10:38 +0530

    Time: 0 ms (100%), Space: 8.4 MB (69.57%) - LeetHub

--- 1970-sorting-the-sentence/1970-sorting-the-sentence.cpp
+++ 1970-sorting-the-sentence/1970-sorting-the-sentence.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     string sortSentence(string s) {
-        vector<string> words(10); // Array to store words in correct order
-        string word = "";
-        int count = 0; // To count number of words
-
-        for (char c : s) {
-            if (c == ' ') {
-                int pos = word.back() - '0'; // Get position from last character
-                word.pop_back(); // Remove the digit
-                words[pos] = word; // Store in correct position
-                word = ""; // Reset word
-                count++; // Increment word count
-            } else {
-                word += c;
-            }
+        string Result = "";
+        string True_result = "" ;
+        vector <string> Words (10 , "") ;
+      for (int st = 0 ; st <= s.length() - 1  ; st += 1)
+      {
+        if (isalpha(s[st]))
+        {
+            Result += s[st] ;
         }
-
-        // For the last word
-        int pos = word.back() - '0';
-        word.pop_back();
-        words[pos] = word;
-        count++;
-
-        // Construct the sorted sentence
-        string result = words[1];
-        for (int i = 2; i <= count; i++) {
-            result += " " + words[i];
+        else if (s[st] - '0' <= 9 && s[st] != ' ')
+        {
+            Words[s[st] - '0'] = Result ;
+            Result = "" ;
         }
-
-        return result;
+      }
+      for (int st = 1  ; st <= Words.size() - 1 ; st += 1)
+      {
+       True_result += Words[st] ;
+       if (st != Words.size() - 1) True_result += ' ' ;
+      }
+      int end = True_result.length() - 1 ;
+      while (True_result[end] == ' ') 
+      {
+        True_result.pop_back() ;
+        end -= 1 ;
+      }
+      return True_result ; ;  
     }
 };
