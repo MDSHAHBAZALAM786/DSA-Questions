7ed418b052d13723a8e3e2d0e3aa49efc43108d0
Md shahbaz alam <112703427+MDSHAHBAZALAM786@users.noreply.github.com>
2025-02-28 17:13:33 +0530

    Time: 16 ms (97.09%), Space: 12.7 MB (99.79%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -1,51 +1,63 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        int m = str1.size(), n = str2.size();
-        
-        // Step 1: Compute LCS using DP
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (str1[i - 1] == str2[j - 1])
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                else
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        int m=str1.length();
+        int n=str2.length();
+
+        int t[1001][1001];
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n+1;j++){
+                if(i==0 || j==0){
+                    t[i][j]=0;
+                }
             }
         }
-        
-        // Step 2: Build SCS using the DP table
-        string result = "";
-        int i = m, j = n;
-        
-        while (i > 0 && j > 0) {
-            if (str1[i - 1] == str2[j - 1]) {
-                result += str1[i - 1];  // If characters match, include them once
-                i--; j--;
-            } 
-            else if (dp[i - 1][j] > dp[i][j - 1]) {
-                result += str1[i - 1];  // Move up in the DP table
+
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(str1[i-1]==str2[j-1]){
+                    t[i][j]=1+t[i-1][j-1];
+                }
+                else{
+                    t[i][j]=max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+
+        string s="";
+        int i=m;
+        int j=n;
+
+        while(i>0 && j>0){
+            if(str1[i-1]==str2[j-1]){
+                s.push_back(str1[i-1]);
                 i--;
-            } 
-            else {
-                result += str2[j - 1];  // Move left in the DP table
                 j--;
             }
+            else{
+                if(t[i-1][j] > t[i][j-1]){
+                    s.push_back(str1[i-1]);
+                    i=i-1;
+                    j=j;
+                }
+                else{
+                    s.push_back(str2[j-1]);
+                    i=i;
+                    j=j-1;
+                }
+            }
         }
-        
-        // Add remaining characters from both strings
-        while (i > 0) {
-            result += str1[i - 1];
+
+        while(i>0){
+            s.push_back(str1[i-1]);
             i--;
         }
-        
-        while (j > 0) {
-            result += str2[j - 1];
+        while(j>0){
+            s.push_back(str2[j-1]);
             j--;
         }
-        
-        reverse(result.begin(), result.end()); // Reverse to get correct order
-        return result;
+
+        reverse(s.begin(),s.end());
+        return s;
     }
 };
