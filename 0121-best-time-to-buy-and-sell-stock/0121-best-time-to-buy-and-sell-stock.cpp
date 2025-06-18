9a4a6f3f05671da3689e82ce3fdddc57425f64a5
Md shahbaz alam <112703427+MDSHAHBAZALAM786@users.noreply.github.com>
2025-06-18 11:39:50 +0530

    Time: 0 ms (100%), Space: 97.5 MB (26.29%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,8 +5,12 @@ public:
         int min=prices[0];
         int ans=0;
         for(int i=1;i<n;i++){
-            if(prices[i]>min) ans=max(ans,prices[i]-min); 
-            else min=prices[i];
+            if(prices[i]>min){
+                ans=max(ans,prices[i]-min);
+            }
+            else{
+                min=prices[i];
+            } 
         }
         return ans;
     
