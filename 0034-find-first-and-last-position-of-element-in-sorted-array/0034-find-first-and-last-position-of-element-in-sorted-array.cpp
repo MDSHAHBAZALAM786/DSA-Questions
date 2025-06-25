cf1817cb23e6284363505c2b81d647bf7671a233
Md shahbaz alam <112703427+MDSHAHBAZALAM786@users.noreply.github.com>
2025-06-25 11:21:17 +0530

    Time: 0 ms (100%), Space: 17.6 MB (52.4%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,21 +1,58 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
+
+            // Linear Search
+        // int n=nums.size();
+        // int left = -1;
+        // int right = -1;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==target){
+        //         left=i;
+        //         break;
+        //     }
+        // }
+        // for(int i=n-1;i>=0;--i){
+        //     if(nums[i]==target){
+        //         right=i;
+        //         break;
+        //     }
+        // }
+        // return{left,right};
+
+
+            // Binary Search
         int n=nums.size();
-        int left = -1;
-        int right = -1;
-        for(int i=0;i<n;i++){
-            if(nums[i]==target){
-                left=i;
-                break;
+        int start=0;
+        int end=n-1;
+        int left= -1;
+        int right= -1;
+        // For right occurrence
+        while(start<= end){
+            int mid=(start+end)/2;
+            if(nums[mid]>target){
+                end=mid-1;
+            }else if(nums[mid]<target){
+                start=mid+1;
+            }else{
+                right=mid;
+                start=mid+1;
             }
         }
-        for(int i=n-1;i>=0;--i){
-            if(nums[i]==target){
-                right=i;
-                break;
+        // For left occurrence
+        start=0;
+        end=n-1;
+        while(start<= end){
+            int mid=(start+end)/2;
+            if(nums[mid]>target){
+                end=mid-1;
+            }else if(nums[mid]<target){
+                start=mid+1;
+            }else{
+                left=mid;
+                end=mid-1;
             }
         }
-        return{left,right};
+        return {left,right};
     }
 };
