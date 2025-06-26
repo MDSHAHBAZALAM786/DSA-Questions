73de47784c52b99ca9989aa182e50fe1f5929d92
Md shahbaz alam <112703427+MDSHAHBAZALAM786@users.noreply.github.com>
2025-06-26 10:05:46 +0530

    Time: 0 ms (100%), Space: 71.8 MB (51.98%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+            // Not Working O(N^3)
+        // int maxSubArrSum = INT_MIN;
+        // int n = nums.size();
+        // for(int i=0;i<n;i++){
+        //     for(int j=i;j<n;j++){
+        //         int currSubArrSum = 0;
+        //         for(int k=i;k<=j;k++){
+        //             currSubArrSum += nums[k]; 
+        //         }
+        //         maxSubArrSum = max(maxSubArrSum, currSubArrSum);
+        //     }
+        // }
+        // return maxSubArrSum;
+
+
+            // Not Working O(N^2)
+        // int n = nums.size();
+        // int maxSubArrSum = INT_MIN;
+        // for(int i=0;i<n;i++){
+        //     int currSubArrSum = 0;
+        //     for(int j=i;j<n;j++){
+        //         currSubArrSum += nums[j];
+        //         maxSubArrSum = max(maxSubArrSum, currSubArrSum);
+        //     }
+        // }
+        // return maxSubArrSum;
+
+
+        // Working
+        int n = nums.size();
+        int maxSubArrSum = INT_MIN;
+        int currSubArrSum = 0;
+        for(int i=0;i<n;i++){
+            currSubArrSum += nums[i];
+            maxSubArrSum = max(maxSubArrSum, currSubArrSum);
+            if(currSubArrSum < 0){
+                currSubArrSum = 0;
+            }
+        }
+        return maxSubArrSum;
+    }
+};
