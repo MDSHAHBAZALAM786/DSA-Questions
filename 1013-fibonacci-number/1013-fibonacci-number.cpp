edb8ffaea6b120803fecf4980a33ae85a741979e
Md shahbaz alam <112703427+MDSHAHBAZALAM786@users.noreply.github.com>
2025-07-11 09:33:03 +0530

    Time: 10 ms (46.65%), Space: 7.7 MB (63.97%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
-        // Recursion
-        if(n<=1){
+        if(n <= 1){
             return n;
         }else{
             return fib(n-1)+fib(n-2);
