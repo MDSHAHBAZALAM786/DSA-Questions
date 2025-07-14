91c36afcf6490ef24df4b16524912c38fd6c2e40
Md shahbaz alam <112703427+MDSHAHBAZALAM786@users.noreply.github.com>
2025-07-14 10:55:03 +0530

    Time: 7 ms (38.03%), Space: 27.6 MB (20.55%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -12,25 +12,35 @@
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        unordered_map<int, int> map;
-        for (int i = 0; i < inorder.size(); ++i) {
-            map[inorder[i]] = i;
+        unordered_map<int, int> valueInorderIndexMap;
+        int n = inorder.size();
+        for (int idx = 0; idx < n; ++idx) {
+            valueInorderIndexMap[inorder[idx]] = idx;
         }
-        return build(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1, map);
+
+        return buildTreeHelper(preorder, inorder, 0, n - 1, 0, n - 1, valueInorderIndexMap);
     }
 
-    TreeNode* build(vector<int>& preorder, int prestart, int preend, 
-                    vector<int>& inorder, int instart, int inend, 
-                    unordered_map<int, int>& map) {
-        if (prestart > preend || instart > inend) return nullptr;
+    TreeNode* buildTreeHelper(vector<int>& preorder, vector<int>& inorder,
+                              int preStart, int preEnd, int inStart, int inEnd,
+                              unordered_map<int, int>& valueInorderIndexMap) {
 
-        TreeNode* node = new TreeNode(preorder[prestart]);
-        int x = map[preorder[prestart]];
-        int y = x - instart;
+        if (preStart > preEnd || inStart > inEnd) {
+            return nullptr;
+        }
 
-        node->left = build(preorder, prestart + 1, prestart + y, inorder, instart, x - 1, map);
-        node->right = build(preorder, prestart + y + 1, preend, inorder, x + 1, inend, map);
+        int rootNodeValue = preorder[preStart];
+        TreeNode* root = new TreeNode(rootNodeValue);
 
-        return node;
+        int inorderIndex = valueInorderIndexMap[rootNodeValue];
+        int countLeftSubTreeNodes = inorderIndex - inStart;
+
+        root->left = buildTreeHelper(preorder, inorder, preStart + 1, preStart + countLeftSubTreeNodes,
+                                     inStart, inorderIndex - 1, valueInorderIndexMap);
+
+        root->right = buildTreeHelper(preorder, inorder, preStart + countLeftSubTreeNodes + 1, preEnd,
+                                      inorderIndex + 1, inEnd, valueInorderIndexMap);
+
+        return root;
     }
 };
