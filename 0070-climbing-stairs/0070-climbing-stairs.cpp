11b601afe8634592cb9d9a902db76a16012e1554
Md shahbaz alam <112703427+MDSHAHBAZALAM786@users.noreply.github.com>
2025-07-17 11:08:10 +0530

    Time: 0 ms (100%), Space: 8.5 MB (36.82%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
-    int climbStairsHelper(int start, int end, vector<int> &dp){
-        if(dp[start] != -1){
-            return dp[start];
-        }
-        dp[start] = climbStairsHelper(start+1, end, dp) + climbStairsHelper(start+2, end, dp);
-        return dp[start];
-    }
+    // int climbStairsHelper(int start, int end, vector<int> &dp){
+    //     if(dp[start] != -1){
+    //         return dp[start];
+    //     }
+    //     dp[start] = climbStairsHelper(start+1, end, dp) + climbStairsHelper(start+2, end, dp);
+    //     return dp[start];
+    // }
 
     int climbStairs(int n) {
-        vector<int> dp(n+1 , -1);
-        dp[n] = 1;
-        dp[n-1] = 1;
-        climbStairsHelper(0, n, dp);
+        // vector<int> dp(n+1 , -1);
+        // dp[n] = 1;
+        // dp[n-1] = 1;
+        // climbStairsHelper(0, n, dp);
+        // return dp[0];
+
+
+        // *************** TABULATION *************
+        vector<int> dp(n+1, -1);
+        dp[n-1] = dp[n] = 1;
+        for (int i = n - 2; i >= 0; --i) {
+            dp[i] = dp[i + 1] + dp[i + 2];
+        }
         return dp[0];
     }
 };
