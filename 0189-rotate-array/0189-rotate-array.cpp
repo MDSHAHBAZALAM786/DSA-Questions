3ec428f3778ff4054b82b22d2ce1bb88d7cfad25
Md shahbaz alam <112703427+MDSHAHBAZALAM786@users.noreply.github.com>
2025-08-14 15:59:23 +0530

    Time: 0 ms (100%), Space: 29.7 MB (37.46%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,17 +1,27 @@
+#include<bits/stdc++.h>
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+
+        // Brute Force
+        // int n = nums.size();
+        // k = k%n;
+        // vector<int> temp(k);
+        // for (int i = 0; i < k; i++) {
+        //     temp[i] = nums[n - k + i];
+        // }
+        // for (int i = n - k - 1; i >= 0; i--) {
+        //     nums[i + k] = nums[i];
+        // }
+        // for (int i = 0; i < k; i++) {
+        //     nums[i] = temp[i];
+        // }
+
+        // Optimal Solution
         int n = nums.size();
         k = k%n;
-        vector<int> temp(k);
-        for (int i = 0; i < k; i++) {
-            temp[i] = nums[n - k + i];
-        }
-        for (int i = n - k - 1; i >= 0; i--) {
-            nums[i + k] = nums[i];
-        }
-        for (int i = 0; i < k; i++) {
-            nums[i] = temp[i];
-        }
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };
