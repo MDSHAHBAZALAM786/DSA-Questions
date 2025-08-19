3ed50c091d97aff9a7488c2643d27b92164b28b9
Md shahbaz alam <112703427+MDSHAHBAZALAM786@users.noreply.github.com>
2025-08-19 17:48:13 +0530

    Time: 0 ms (100%), Space: 21.8 MB (70.21%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        // int n = nums.size();
+        // vector<int> hash(n+2,0);
+        // for(int i=0;i<n;i++){
+        //     hash[nums[i]]  = 1;
+        // }
+        // for(int i=0;i<=n;i++){
+        //     if(hash[i]==0){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+            // Optimal Solution
         int n = nums.size();
-        vector<int> hash(n+2,0);
+        int xor1 = 0;
+        int xor2 = 0;
         for(int i=0;i<n;i++){
-            hash[nums[i]]  = 1;
+            xor2 = xor2^nums[i];
+            xor1 = xor1^i;
         }
-        for(int i=0;i<=n;i++){
-            if(hash[i]==0){
-                return i;
-            }
-        }
-        return -1;
+        xor1 = xor1^n;
+        return xor1^xor2;
     }
 };
