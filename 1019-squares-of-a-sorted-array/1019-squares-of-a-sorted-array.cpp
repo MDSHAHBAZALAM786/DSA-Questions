ea131785f58455841dcf3a3e038945caaff22568
Md shahbaz alam <112703427+MDSHAHBAZALAM786@users.noreply.github.com>
2025-08-27 21:51:13 +0530

    Time: 14 ms (10.42%), Space: 30.3 MB (63.84%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++){
-            nums[i] = nums[i]*nums[i];
+        // Brute force
+        // for(int i=0;i<nums.size();i++){
+        //     nums[i] = nums[i]*nums[i];
+        // }
+        // sort(nums.begin(),nums.end());
+        // return nums;
+
+        // Optimal Solution
+        vector<int> temp;
+        temp.reserve(nums.size());
+        for(int i:nums){
+            temp.push_back(i*i);
         }
-        sort(nums.begin(),nums.end());
-        return nums;
+        sort(temp.begin(),temp.end());
+        return temp;
     }
 };
