bdb023fc387d23e4f1f841b378ed45d123554785
Md shahbaz alam <112703427+MDSHAHBAZALAM786@users.noreply.github.com>
2025-08-30 18:03:27 +0530

    Time: 0 ms (100%), Space: 11.6 MB (78.95%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-       int red=0,white=0,black=0;
-       for(int x: nums){
-        if(x==0) red++;
-        else if(x==1) white++;
-        else black++;
-       }
-        int index=0;
-        while(red>0){
-            nums[index]=0;
-            index++;
-            --red;
-        }
-        while(white>0){
-            nums[index++]=1;
-            white--;
-        }
-        while(black>0){
-            nums[index++]=2;
-            black--;
-        }
+    //    int red=0,white=0,black=0;
+    //    for(int x: nums){
+    //     if(x==0) red++;
+    //     else if(x==1) white++;
+    //     else black++;
+    //    }
+    //     int index=0;
+    //     while(red>0){
+    //         nums[index]=0;
+    //         index++;
+    //         --red;
+    //     }
+    //     while(white>0){
+    //         nums[index++]=1;
+    //         white--;
+    //     }
+    //     while(black>0){
+    //         nums[index++]=2;
+    //         black--;
+    //     }
+    sort(nums.begin(),nums.end());
     }
 };
