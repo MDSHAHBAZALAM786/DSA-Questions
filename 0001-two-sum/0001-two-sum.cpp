f27cdc5e68e8a22c8ee021eb81b4183217c0b7a9
Md shahbaz alam <112703427+MDSHAHBAZALAM786@users.noreply.github.com>
2025-08-30 17:52:18 +0530

    Time: 4 ms (57.04%), Space: 14.9 MB (20.37%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // Brute Force
+        // int n = nums.size();
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums[i]+nums[j]==target){
+        //             return{i,j};
+        //         }
+        //     }
+        // }
+        // return{};
+
+        // Better Solution
         int n = nums.size();
+        map<int,int> mpp;
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                    return{i,j};
-                }
+            int a = nums[i];
+            int more = target-a;
+            if(mpp.find(more) != mpp.end()){
+                return {mpp[more],i};
             }
+            mpp[a] = i;
         }
-        return{};
+        return {};
     }
 };
